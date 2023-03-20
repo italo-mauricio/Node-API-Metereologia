9c52396503af478f88ea571dd855887a875c12e0
italo <italogithub@gmail.com>
2023-03-20 19:55:00 -0300

    preparando o controller

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,11 +1,10 @@
 const { resolve } = require('path');
-const { displayName } = require('../jest.config');
 const root = resolve(__dirname, '..');
 const rootConfig = require(`${root}/jest.config.js`);
 
-module.require = { ... rootConfig, ... {
-    rootDir: root,
-    displayName: "end2end-tests",
-    setupFilesAfterEnv: ["<rootDir>/test/jest-setup.ts"],
-    testMatch: ["<rootDir>/test/**/*.tests.ts"],
+module.exports = {...rootConfig, ...{
+  rootDir: root,
+  displayName: "end2end-tests",
+  setupFilesAfterEnv: ["<rootDir>/test/jest-setup.ts"],
+  testMatch: ["<rootDir>/test/**/*.test.ts"],
 }}
